d24f6f062f3719b74545fd4a3b55fdef9d0cc14a
Leo-de-Riv3r <leoguti182@gmail.com>
2025-08-07 00:19:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const http = require('http')
+const express = require('express')
+const morgan = require('morgan');
+const app = express()
+//permite recibir datos en formato json
+app.use(express.json())
+// Usar morgan en modo 'dev' (colores y detalles resumidos)
+app.use(morgan('tiny'));
+
+let contacts = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/contacts', (request, response) => {
+  response.json(contacts)
+})
+
+app.get('/api/contacts/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = contacts.find(note => note.id === id)
+   if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const maxId = contacts.length > 0
+    ? Math.max(...contacts.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+// app.post('/api/contacts', (request, response) => {
+//   const body = request.body
+
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   }
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+// })
+
+//middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
